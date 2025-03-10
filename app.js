106c67d4bedc20697ea1cc49a67332bcc26b27a8
yared <yared@gmail.com>
2025-03-10 15:38:26 -0400

    pushed

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port,()=>{
+   console.log("server running on port ",ProgressEvent);
+});
+
+const io =  require('socket.io')(server);
+
+io.on('connection',(socket)=>{
+    console.log("connected successfully...");
+});
